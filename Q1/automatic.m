9d6e3bc428ffd33e403979786d0afb195992ea7c
Sagar <sagarpatel9410@gmail.com>
2017-04-12 11:12:22 +0100

    harris

--- Q1/automatic.m
+++ Q1/automatic.m
@@ -5,5 +5,14 @@ srcFiles = dir('Image-Data\tsukuba\*.ppm');
 for i = 1:length(srcFiles)
   directory = strcat('Image-Data\tsukuba\', srcFiles(i).name);
   images{i} = imread(directory);
-end
 
+  %convert image to grayscale
+  images{i} = rgb2gray(images{i});
+end
+% 
+%   points = cornerPoints(harris(images{1},0.04,0.01));
+%   imshow(images{1})
+%   hold on
+%   plot(points);
+  
+  
